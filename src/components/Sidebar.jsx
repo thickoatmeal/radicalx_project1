7a45c06d8e7023c92daad46d3356ecde2d23df8f
thickoatmeal <59904775+thickoatmeal@users.noreply.github.com>
2023-01-26 10:10:06 -0800

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import NavLink from "./NavLink";
+import "../styles/sidebar.css";
+// Image imports
+import dashboardIcon from "../assets/dashboard-3.svg";
+import radicalLogo from "../assets/RadicallX-Black-Logo 1.svg";
+import apprenLogo from "../assets/medal-star.svg";
+import internLogo from "../assets/book.svg";
+import jobsLogo from "../assets/briefcase.svg";
+import settingsLogo from "../assets/setting-2.svg";
+import adamScott from "../assets/Rectangle 18895.png";
+
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <img src={radicalLogo} className="nav-logo" alt="RadicalX" />
+      <NavLink title="Dashboard" image={dashboardIcon} />
+      <NavLink title="Apprenticeships" image={apprenLogo} />
+      <NavLink title="Internships" image={internLogo} isActive />
+      <NavLink title="Jobs" image={jobsLogo} />
+      <NavLink title="Settings" image={settingsLogo} />
+      <div className="nav-footer">
+        
+        <NavLink title="Adam Scott" image ={adamScott} />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
